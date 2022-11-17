9e8f621041628f35f7be5e21b33ac65bd05e1b3c
Roy Wang <smokingpc@gmail.com>
2022-11-17 15:36:11 +0800

    finish ServiceDependentTool

--- src/CSharp_NetCore6/ServiceDependentTool/frmMain.Designer.cs
+++ src/CSharp_NetCore6/ServiceDependentTool/frmMain.Designer.cs
@@ -28,11 +28,237 @@ partial class frmMain
     /// </summary>
     private void InitializeComponent()
     {
-        this.components = new System.ComponentModel.Container();
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(800, 450);
-        this.Text = "Form1";
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridViewCheckBoxColumn1 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(291, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Please select your primary service";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 345);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(568, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Please select service(s) which you want depend on primary service.";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 36);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(960, 290);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column1.Width = 32;
+            // 
+            // Column2
+            // 
+            this.Column2.DataPropertyName = "DisplayName";
+            this.Column2.HeaderText = "DisplayName";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Width = 360;
+            // 
+            // Column3
+            // 
+            this.Column3.DataPropertyName = "ServiceName";
+            this.Column3.HeaderText = "ServiceName";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Width = 240;
+            // 
+            // Column4
+            // 
+            this.Column4.DataPropertyName = "StartType";
+            this.Column4.HeaderText = "Start Type";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            this.Column4.Width = 160;
+            // 
+            // Column5
+            // 
+            this.Column5.DataPropertyName = "Status";
+            this.Column5.HeaderText = "Status";
+            this.Column5.MinimumWidth = 6;
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            this.Column5.Width = 160;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.AllowUserToResizeRows = false;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewCheckBoxColumn1,
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3,
+            this.dataGridViewTextBoxColumn4});
+            this.dataGridView2.Location = new System.Drawing.Point(12, 372);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersVisible = false;
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 29;
+            this.dataGridView2.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView2.Size = new System.Drawing.Size(960, 290);
+            this.dataGridView2.TabIndex = 3;
+            this.dataGridView2.SelectionChanged += new System.EventHandler(this.dataGridView2_SelectionChanged);
+            // 
+            // dataGridViewCheckBoxColumn1
+            // 
+            this.dataGridViewCheckBoxColumn1.HeaderText = "";
+            this.dataGridViewCheckBoxColumn1.MinimumWidth = 6;
+            this.dataGridViewCheckBoxColumn1.Name = "dataGridViewCheckBoxColumn1";
+            this.dataGridViewCheckBoxColumn1.ReadOnly = true;
+            this.dataGridViewCheckBoxColumn1.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridViewCheckBoxColumn1.Width = 32;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "DisplayName";
+            this.dataGridViewTextBoxColumn1.HeaderText = "DisplayName";
+            this.dataGridViewTextBoxColumn1.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.ReadOnly = true;
+            this.dataGridViewTextBoxColumn1.Width = 360;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.DataPropertyName = "ServiceName";
+            this.dataGridViewTextBoxColumn2.HeaderText = "ServiceName";
+            this.dataGridViewTextBoxColumn2.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            this.dataGridViewTextBoxColumn2.ReadOnly = true;
+            this.dataGridViewTextBoxColumn2.Width = 240;
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.DataPropertyName = "StartType";
+            this.dataGridViewTextBoxColumn3.HeaderText = "Start Type";
+            this.dataGridViewTextBoxColumn3.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            this.dataGridViewTextBoxColumn3.ReadOnly = true;
+            this.dataGridViewTextBoxColumn3.Width = 160;
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.DataPropertyName = "Status";
+            this.dataGridViewTextBoxColumn4.HeaderText = "Status";
+            this.dataGridViewTextBoxColumn4.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            this.dataGridViewTextBoxColumn4.ReadOnly = true;
+            this.dataGridViewTextBoxColumn4.Width = 160;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(799, 668);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(173, 41);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "SET Dependency";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(982, 36);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(354, 673);
+            this.textBox1.TabIndex = 5;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1348, 721);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "frmMain";
+            this.Text = "Service Dependency Tool";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
     }
 
     #endregion
+    private Label label1;
+    private Label label2;
+    private DataGridView dataGridView1;
+    private DataGridView dataGridView2;
+    private Button button1;
+    private DataGridViewCheckBoxColumn Column1;
+    private DataGridViewTextBoxColumn Column2;
+    private DataGridViewTextBoxColumn Column3;
+    private DataGridViewTextBoxColumn Column4;
+    private DataGridViewTextBoxColumn Column5;
+    private DataGridViewCheckBoxColumn dataGridViewCheckBoxColumn1;
+    private DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+    private DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+    private DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+    private DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+    private TextBox textBox1;
 }
--- src/CSharp_NetCore6/ServiceDependentTool/frmMain.cs
+++ src/CSharp_NetCore6/ServiceDependentTool/frmMain.cs
@@ -1,9 +1,80 @@
+using System.ComponentModel;
+using System.ServiceProcess;
+using System.Diagnostics;
+
 namespace ServiceDependentTool;
 
 public partial class frmMain : Form
 {
+    ServiceController PrimarySvc = null;
+    List<ServiceController> ChildSvcList = new List<ServiceController>();
+
     public frmMain()
     {
         InitializeComponent();
+    }
+
+    private void frmMain_Load(object sender, EventArgs e)
+    {
+        //I only want to show fields which I designed on UI.
+        //Don't auto create new column for me....
+        dataGridView1.AutoGenerateColumns = false;
+
+        List<ServiceController> result = new List<ServiceController>();
+        result.AddRange(ServiceController.GetServices());
+
+        BindingList<ServiceController> binding = new BindingList<ServiceController>(result);
+        dataGridView1.DataSource = binding;
+    }
+
+    private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+    {
+        PrimarySvc = null;
+        foreach (DataGridViewRow row in dataGridView1.Rows)
+        {
+            if (dataGridView1.SelectedRows.Count > 0 && dataGridView1.SelectedRows[0] == row)
+            {
+                row.Cells[0].Value = true;
+                PrimarySvc = (ServiceController)row.DataBoundItem;
+            }
+            else
+                row.Cells[0].Value = false;
+        }
+    }
+
+    private void dataGridView2_SelectionChanged(object sender, EventArgs e)
+    {
+        ChildSvcList.Clear();
+        foreach (DataGridViewRow row in dataGridView1.Rows)
+        {
+            if (dataGridView1.SelectedRows.Count > 0 && dataGridView1.SelectedRows[0] == row)
+            {
+                row.Cells[0].Value = true;
+                ChildSvcList.Add((ServiceController)row.DataBoundItem);
+            }
+            else
+                row.Cells[0].Value = false;
+        }
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+        if (ChildSvcList.Count > 0)
+        { 
+            //sc.exe的所有參數等號後面一定要有個空格，否則命令會失敗
+            foreach (var svc in ChildSvcList)
+            {
+                string cmd = $"sc config {svc.ServiceName} depend= {PrimarySvc.ServiceName}";
+                Process shell = new Process();
+                shell.StartInfo.FileName = "cmd.exe";
+                shell.StartInfo.RedirectStandardOutput = true;
+                shell.StartInfo.CreateNoWindow = true;
+                shell.StartInfo.UseShellExecute = false;
+                shell.Start();
+
+                shell.WaitForExit();
+                textBox1.AppendText(shell.StandardOutput.ReadToEnd() + "\r\n");
+            }
+        }
     }
 }
