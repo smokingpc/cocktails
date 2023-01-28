ef519b7dd3afdfbeb6b28e8c87a90db01538c2a4
Roy Wang <smokingpc@gmail.com>
2023-01-28 20:57:41 +0800

    update SimpleCrashTest

--- src/CppTempTest/SimpleCrashTest/SimpleCrashTest.cpp
+++ src/CppTempTest/SimpleCrashTest/SimpleCrashTest.cpp
@@ -1,5 +1,7 @@
-#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
 
+#include <stdio.h>
+#include <string.h>
 typedef struct _TEST_DATA1 {
     char Name[64];
 }TEST_DATA1;
@@ -13,6 +15,6 @@ int main()
     TEST_DATA1 *ptr1 = NULL;
     TEST_DATA2 data = {0};
     data.Number = 12345678;
-
+    strcat(ptr1->Name,"Test");
     printf("Name=%s\n", ptr1->Name);
 }
