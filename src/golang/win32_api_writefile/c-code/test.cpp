bd9362cdb8cab82334a6908973ab1dbefc2a504b
Roy Wang <roy.wang@graidtech.com>
2023-04-24 18:28:30 +0800

    cleanup golong win32_api_writefile files

--- src/golang/win32_api_writefile/c-code/test.cpp
+++
@@ -1,7 +0,0 @@
-#include "test.h"
-#pragma lib("user32.lib")
-
-void TestMsg(TCHAR *msg)
-{
-    MessageBox(NULL, msg, NULL, MB_OK);
-}
--- src/golang/win32_api_writefile/c-code/test.h
+++
@@ -1,7 +0,0 @@
-#include <tchar.h>
-#include <windows.h>
-
-//void TestMsg(TCHAR *msg);
-typedef struct _MY_DATA{
-    int id;
-} MY_DATA;
