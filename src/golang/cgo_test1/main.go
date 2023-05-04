31ba147501ecaf7576425820eb776b559bba5f54
SmokingPC <smokingpc@gmail.com>
2023-05-04 16:17:24 +0800

    fix cgo_test1

--- src/golang/cgo_test1/main.go
+++ src/golang/cgo_test1/main.go
@@ -5,6 +5,10 @@ package main
 */
 import "C"
 
+//目前cgo不支援 MSVC，必須去抓 MingW-Win64 套件來用，裡面有包含gcc.exe與g++.exe
+//https://github.com/niXman/mingw-builds-binaries/releases
+//Build Machine要注意在script裡切換 CC 與 CXX 環境變數，免得干擾到其他MSVC project編譯
+
 func main() {
-	C.TestMsg(C.CString("28825252"))
+	C.TestMsg(C.CString("12345678"))
 }
