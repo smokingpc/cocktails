96f0abe568a3edaeb911ca1c9a7dd5cf21950ad4
Roy Wang <roy.wang@graidtech.com>
2023-05-09 17:20:12 +0800

    add win32_enum_disks for golang

---
+++ src/golang/win32_read_physical_disk/go.mod
@@ -0,0 +1,5 @@
+module win32_read_physical_disk
+
+go 1.20
+
+require golang.org/x/sys v0.8.0 // indirect
---
+++ src/golang/win32_read_physical_disk/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+
+	native "golang.org/x/sys/windows"
+)
+
+func main() {
+	var diskpath string = "\\\\.\\PhysicalDrive0"
+	var buffer []byte = make([]byte, 4096)
+	path, _ := native.UTF16PtrFromString(diskpath)
+	handle, _ := native.CreateFile(
+		path,
+		native.GENERIC_READ,
+		native.FILE_SHARE_READ|native.FILE_SHARE_WRITE,
+		nil,
+		native.OPEN_EXISTING,
+		0,
+		0)
+
+	native.ReadFile(handle, buffer, nil, nil)
+	fmt.Printf("[1st]\n%v\n", buffer)
+	native.ReadFile(handle, buffer, nil, nil)
+	fmt.Printf("[2nd]\n%v\n", buffer)
+	native.ReadFile(handle, buffer, nil, nil)
+	fmt.Printf("[3rd]\n%v\n", buffer)
+
+	native.CloseHandle(handle)
+}
