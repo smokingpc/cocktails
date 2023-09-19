fac05b4fd0078b3b85dbf2fcb85d38162954a703
SmokingPC <smokingpc@gmail.com>
2023-09-19 16:49:34 +0800

    DumpPhysicalDiskData supports offset

--- src/Cpp/DumpPhysicalDiskData/DumpPhysicalDiskData.cpp
+++ src/Cpp/DumpPhysicalDiskData/DumpPhysicalDiskData.cpp
@@ -4,18 +4,19 @@
 #include <windows.h>
 #include <string>
 
-#define SIZE_1MB    1048576
-#define SIZE_256KB    (1024*256)
-#define SIZE_4KB    (4096)
+#define SIZE_1KB    1024
+#define SIZE_1MB    (SIZE_1KB*SIZE_1KB)
+#define SIZE_256KB  (SIZE_1KB*256)
+#define SIZE_4KB    (4*SIZE_1KB)
 
 void Usage()
 {
-    printf("Usage: DumpPhysicalDiskData.exe [physical_drive_devpath] [dump_size_in_MB] [output_file_fullpath]\n");
+    printf("Usage: DumpPhysicalDiskData.exe [physical_drive_devpath] [begin_offset_in_4KB] [dump_size_in_4KB] [output_file_fullpath]\n");
     printf("e.g.: If you want to dump first 3MB of physical disk 1 to c:\\test\\dumpdata.bin ,\n");
-    printf("      you should use command: DumpPhysicalDiskData.exe \\\\.\\PhysicalDrive1 3 c:\\test\\dumpdata.bin \n\n");
+    printf("      you should use command: DumpPhysicalDiskData.exe \\\\.\\PhysicalDrive1 0 3072 c:\\test\\dumpdata.bin \n\n");
 }
 
-void DumpDataFromPhysicalDisk(char *disk_devpath, int size_in_mb, char *output_file)
+void DumpDataFromPhysicalDisk(char *disk_devpath, LARGE_INTEGER offset, UINT32 dump_size_4kb, char *output_file)
 {
     HANDLE disk = CreateFileA(disk_devpath, GENERIC_READ, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, 0, NULL);
     HANDLE file = CreateFileA(output_file, GENERIC_WRITE, FILE_SHARE_READ , NULL, CREATE_ALWAYS, 0, NULL);
@@ -24,33 +25,39 @@ void DumpDataFromPhysicalDisk(char *disk_devpath, int size_in_mb, char *output_f
     {
         if(file != INVALID_HANDLE_VALUE)
         {
-            //UCHAR *buffer = new UCHAR[SIZE_1MB];
-            UCHAR *buffer = new UCHAR[SIZE_4KB];
-            int size_in_4kb = (size_in_mb * SIZE_1MB)/ SIZE_4KB;
-            //for(int i=0; i<size_in_mb; i++)
-            printf("size_in_mb=%d, size_in_4kb=%d\n", size_in_mb, size_in_4kb);
-            for (int i = 0; i < size_in_4kb; i++)
+            printf("Try to set disk read offset(%lld)\n", offset.QuadPart);
+            DWORD seek_err = SetFilePointer(disk, offset.LowPart, &offset.HighPart, FILE_BEGIN);
+            if(INVALID_SET_FILE_POINTER == seek_err)
             {
-                DWORD ret_size = 0;
-                DWORD written_size = 0;
-                memset(buffer, 0, SIZE_4KB);
-                BOOL ok = ReadFile(disk, buffer, SIZE_4KB, &ret_size, NULL);
-                if(!ok || 0 == ret_size)
-                {
-                    printf("Read disk failed, LastError=%d\n", GetLastError());
-                    break;
-                }
-                ok = WriteFile(file, buffer, ret_size, &written_size, NULL);
-                if(!ok)
-                {
-                    printf("Write to file failed, LastError=%d\n", GetLastError());
-                    break;
-                }
-
-                counter++;
+                printf("SeekFile failed, LastError(%d)\n", GetLastError());
             }
-            delete[] buffer;
+            else
+            {
+                printf("begin to dump data from (%s) offset(%llX), dump size(%X in 4KB)\n",
+                    disk_devpath, offset.QuadPart, dump_size_4kb);
+                for (int i = 0; i < dump_size_4kb; i++)
+                {
+                    BYTE buffer[SIZE_4KB] = { 0 };
+                    DWORD ret_size = 0;
+                    DWORD written_size = 0;
+                    BOOL ok = ReadFile(disk, buffer, SIZE_4KB, &ret_size, NULL);
+                    if (!ok || 0 == ret_size)
+                    {
+                        printf("Read disk failed, LastError=%d\n", GetLastError());
+                        break;
+                    }
+                    printf("Offset(%llX) read , write to file.\n",
+                        offset.QuadPart + i * SIZE_4KB);
+                    ok = WriteFile(file, buffer, ret_size, &written_size, NULL);
+                    if (!ok)
+                    {
+                        printf("Write to file failed, LastError=%d\n", GetLastError());
+                        break;
+                    }
 
+                    counter++;
+                }
+            }
             CloseHandle(file);
         }
         else
@@ -60,12 +67,12 @@ void DumpDataFromPhysicalDisk(char *disk_devpath, int size_in_mb, char *output_f
     else
         printf("Open Disk(%s) FAILED!!\n", disk_devpath);
 
-    printf("Total Read/Write data size = %d blocks(1 block = 4K)\n", counter);
+    printf("Total Read/Write data size = %d blocks(1 block = 4KB)\n", counter);
 }
 
 int main(int argc, char *argv[])
 {
-    if (argc < 4)
+    if (argc < 5)
     {
         Usage();
         return -1;
@@ -73,9 +80,18 @@ int main(int argc, char *argv[])
 
     //physical disk symbolic link name, e.g. "\\.\PhysicalDrive4"
     std::string devpath = argv[1];
-    int size_mb = atoi(argv[2]);
-    std::string filepath = argv[3];
+    int64_t offset_4kb = _atoi64(argv[2]);
+    int size_4kb = atoi(argv[3]);
+    std::string filepath = argv[4];
+    LARGE_INTEGER offset = {0};
+    offset.QuadPart = offset_4kb * SIZE_4KB;
+    printf("Try to dump %s offset(%lld) size(%d)\n", 
+        devpath.c_str(), offset.QuadPart, size_4kb*SIZE_4KB);
 
-    DumpDataFromPhysicalDisk((char*)devpath.c_str(), size_mb, (char*)filepath.c_str());
+    DumpDataFromPhysicalDisk(
+            (char*)devpath.c_str(), 
+            offset, 
+            size_4kb, 
+            (char*)filepath.c_str());
 }
 
