a5f199ee12614ce2b58b3e16c238ba5389e69310
SmokingPC <smokingpc@gmail.com>
2024-07-29 17:39:19 +0800

    add ManifestTest

---
+++ src/Cpp-Win32Mechanism/ManifestTest/ManifestTest.cpp
@@ -0,0 +1,12 @@
+// ManifestTest.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <tchar.h>
+#include <windows.h>
+
+int _tmain(int argc, TCHAR* argv[])
+{
+    _tprintf(_T("Hello World.\n"));
+
+    return 0;
+}
