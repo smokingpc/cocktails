6b862e503544afef70cdb2f2e08f4c5388ba98d0
Roy Wang <smokingpc@gmail.com>
2024-12-25 17:54:19 +0800

    update

--- src/Cpp-STL/UniquePtr-Test/UniquePtr-Test.cpp
+++ src/Cpp-STL/UniquePtr-Test/UniquePtr-Test.cpp
@@ -3,6 +3,7 @@
 
 #include <tchar.h>
 #include <memory>
+#include <string>
 #include <windows.h>
 
 int _tmain(int argc, TCHAR* argv[])
@@ -12,6 +13,16 @@ int _tmain(int argc, TCHAR* argv[])
         std::unique_ptr<char> ptr(buffer);
         char *cursor = ptr.get();
         RtlZeroMemory(cursor, 1024);
+    }
+
+    DWORD size_cch = 1024;
+    TCHAR *buffer2 = new TCHAR[size_cch];
+    std::basic_string<TCHAR> str;
+    {
+        std::unique_ptr<TCHAR> ptr(buffer2);
+        RtlZeroMemory(ptr.get(), size_cch*sizeof(TCHAR));
+        _tcscat_s(ptr.get(), size_cch, _T("test1234"));
+        str = ptr.get();
     }
 
     return 0;
