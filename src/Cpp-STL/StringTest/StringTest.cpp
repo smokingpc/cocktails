4af89eca057baef728457f66680537daa716e26b
smokingpc <smokingpc@gmail.com>
2026-04-03 17:50:32 +0800

    add STL string test

---
+++ src/Cpp-STL/StringTest/StringTest.cpp
@@ -0,0 +1,15 @@
+// StringTest.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    string data = R"({"name": "aa", "phone":"\XXXX\"})";
+    cout << data << "\n";
+//    std::cout << "Hello World!\n";
+}
